d899e6d2e2178b5f2f402240c8cc8afce035d148
KhaydarovaDiana <80327930+KhaydarovaDiana@users.noreply.github.com>
2021-03-09 20:00:01 +0500

    Update problem4.py

--- hw2/problem4.py
+++ hw2/problem4.py
@@ -4,5 +4,5 @@ s=input()
 r=[None]
 for ch in s:
   if ch !=r[-1]: r.append(ch)
-s_new=''.join(r[1:]) #чем можно заменить join? и за что он отвечает в этом коде, недоконца понятно
+s_new=''.join(r[1:]) #чем можно заменить эту строку
 print(s_new) 
